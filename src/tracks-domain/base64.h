3d80c723ee6e3247e99567858adc4f44cc551c97
Wouter Saaltink <wouter.saaltink@gmail.com>
2022-09-03 14:42:46 +0200

    code format

--- src/tracks-domain/base64.h
+++ src/tracks-domain/base64.h
@@ -1,11 +1,15 @@
 #ifndef _BASE64_H_
 #define _BASE64_H_
 
-#include <vector>
 #include <string>
+#include <vector>
 typedef unsigned char BYTE;
 
-std::string base64_encode(BYTE const* buf, unsigned int bufLen);
-std::vector<BYTE> base64_decode(std::string const&);
+std::string base64_encode(
+    BYTE const *buf,
+    intptr_t bufLen);
+
+std::vector<BYTE> base64_decode(
+    std::string const &);
 
 #endif
